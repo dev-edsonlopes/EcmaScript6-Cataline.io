a9b27bbd317fb7571940223c2b2b6db57547fc19
EdsonLopesJr <dev_edson@outlook.com>
2023-06-24 14:09:22 -0300

    Criando Metodos CRUD

--- src/api/todos.js
+++ src/api/todos.js
@@ -2,7 +2,21 @@ import axios from '../utils/axios'
 
 export default class Todos {
     async index() {
-        const response = await axios.get('/todos') // teste GET
-        return response
+        const { data } = await axios.get('/todos') // teste GET
+        return data
+    }
+
+    async store({ text, done }) {
+        const { data } = await axios.post('/todos', { text, done })
+        return data
+    }
+
+    async update({ id, text, done }) {
+        const { data } = await axios.put(`/todos/${id}`, { text, done})
+        return data
+    }
+
+    async destroy({ id }) {
+        await axios.delete(`/todos/${id}`)
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Todos from "./api/todos";
 
 async function exec() {
-    const todos = Todos()
+    const todos = new Todos()
     const response = await todos.index()
+    console.log("---- Listar ----")
+    console.log(response)
+
+    const create = await todos.store({ 
+        text: "Passeio com DOG", 
+        done: false 
+    })
+    console.log("---- Criar ----")
+    console.log(create)
+
+    const update = await todos.update({ 
+        id: 3,
+        text: "Passear com o DOG de 14hrs",
+        done: false
+    })
+    console.log("---- Atualizar ----")
+    console.log(update)
+    
+    const del = await todos.destroy({ id: 3})
+    console.log("---- Apagar ----")
     console.log(response)
 }
 
